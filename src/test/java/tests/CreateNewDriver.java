222291bbe204a2bb41ce09383ca8f4cbaff9f30a
mindtekbatch15 <arslan@mindtekbootcamp.com>
2026-04-01 19:56:03 -0500

    Viber and Other fields positive tests are added

--- src/test/java/tests/CreateNewDriver.java
+++ src/test/java/tests/CreateNewDriver.java
@@ -92,7 +92,7 @@ public class CreateNewDriver extends TestBase {
         WebElement medicalLicExp = driver.findElement(By.name("medical_certification_exp"));
         medicalLicExp.click();
         medicalLicExp.sendKeys("07232028");
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/main/div[2]/div/div/div/form/div[19]/button")).click();
+        driver.findElement(By.xpath("//button[text()='Create new']")).click();
         String expectedResult = "String must contain at most 50 character(s)";
         String actualResult = driver.findElement(By.xpath("//*[@id=\"uniforms-0003-0002-helper-text\"]")).getText();
         Assert.assertEquals(actualResult, expectedResult);
@@ -147,7 +147,7 @@ public class CreateNewDriver extends TestBase {
         WebElement medicalLicExp = driver.findElement(By.name("medical_certification_exp"));
         medicalLicExp.click();
         medicalLicExp.sendKeys("07232028");
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/main/div[2]/div/div/div/form/div[19]/button")).click();
+        driver.findElement(By.xpath("//button[text()='Create new']")).click();
         String expectedResult = "Required";
         String actualResult = driver.findElement(By.xpath("//input[@name='full_name']/../following-sibling::p")).getText();
         Assert.assertEquals(actualResult, expectedResult);
@@ -168,7 +168,7 @@ public class CreateNewDriver extends TestBase {
         WebElement medicalLicExp = driver.findElement(By.name("medical_certification_exp"));
         medicalLicExp.click();
         medicalLicExp.sendKeys("07232028");
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/main/div[2]/div/div/div/form/div[19]/button")).click();
+        driver.findElement(By.xpath("//button[text()='Create new']")).click();
         String expectedResult = "Input must contain only alphanumeric and specific punctuation characters";
         String actualResult = driver.findElement(By.xpath("//input[@name='full_name']/../following-sibling::p")).getText();
         Assert.assertEquals(actualResult, expectedResult);
@@ -194,7 +194,7 @@ public class CreateNewDriver extends TestBase {
         WebElement medicalLicExp = driver.findElement(By.name("medical_certification_exp"));
         medicalLicExp.click();
         medicalLicExp.sendKeys("07232028");
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/main/div[2]/div/div/div/form/div[19]/button")).click();
+        driver.findElement(By.xpath("//button[text()='Create new']")).click();
         driver.findElement(By.xpath("//button[text()='Go to Edit']")).click();
         // Get Driver ID
         String driverId = driver.findElement(By.xpath("//label[text()='Manual id']/following-sibling::div/input")).getAttribute("value");
@@ -209,6 +209,7 @@ public class CreateNewDriver extends TestBase {
         String actualSelectedState = driver.findElement(By.xpath("//label[text()='Local state *']/following-sibling::div/div")).getText();
         Assert.assertEquals(actualSelectedState, selectedState);
     }
+
     @Test
     public void createDriverPhoneTestPositive() throws InterruptedException {
         driver.get(ConfigReader.getProperty("elarappUrl"));
@@ -219,7 +220,7 @@ public class CreateNewDriver extends TestBase {
         driver.findElement(By.xpath("//button[text()='Add driver']")).click();
         driver.findElement(By.xpath("//input[@name='full_name']")).sendKeys("Daniel Doe");
         driver.findElement(By.xpath("//p[text()='Phone']/following-sibling::*[1]")).click();
-        String phoneNumber=CommonMethods.generatePhoneNumber();
+        String phoneNumber = CommonMethods.generatePhoneNumber();
         driver.findElement(By.xpath("//input[@type='tel']")).sendKeys(phoneNumber);
         driver.findElement(By.xpath("//input[@type='string']")).sendKeys("256");
         WebElement driverLicExp = driver.findElement(By.name("driving_license_exp"));
@@ -238,10 +239,9 @@ public class CreateNewDriver extends TestBase {
         driver.findElement(By.xpath("//label[text()='Search...']/following-sibling::div/input")).sendKeys(phoneNumber + Keys.ENTER);
         driver.findElement(By.xpath("//div[text()='Non staff']")).click();
         Thread.sleep(2000);
-        String phoneExpecte = "+1 "+phoneNumber;
-        String actualPhoneNumber=driver.findElement(By.xpath("//div[@data-field='phone' and @role='cell']/div")).getText();
-
-        Assert.assertEquals(actualPhoneNumber,phoneExpecte);
+        String phoneExpecte = "+1 " + phoneNumber;
+        String actualPhoneNumber = driver.findElement(By.xpath("//div[@data-field='phone' and @role='cell']/div")).getText();
+        Assert.assertEquals(actualPhoneNumber, phoneExpecte);
     }
 
     @Test
@@ -275,8 +275,75 @@ public class CreateNewDriver extends TestBase {
         driver.findElement(By.xpath("//button[text()='Email/Phone']")).click();
         driver.findElement(By.xpath("//label[text()='Search...']/following-sibling::div/input")).sendKeys(emailUser + Keys.ENTER);
         driver.findElement(By.xpath("//div[text()='Non staff']")).click();
-
-        Assert.assertEquals(emailUser,expectedEmail);
-
+        Assert.assertEquals(emailUser, expectedEmail);
     }
+
+    @Test
+    public void createDriverViberTestPositive() throws InterruptedException {
+        driver.get(ConfigReader.getProperty("elarappUrl"));
+        driver.findElement(By.id("login-username")).sendKeys(ConfigReader.getProperty("elarUsername"));
+        driver.findElement(By.id("login-password")).sendKeys(ConfigReader.getProperty("elarPassword"));
+        driver.findElement(By.xpath("//*[@id=\"login-form\"]/div/button")).click();
+        driver.findElement(By.xpath("//a[@href='/drivers/list']")).click();
+        driver.findElement(By.xpath("//button[text()='Add driver']")).click();
+        driver.findElement(By.xpath("//input[@name='full_name']")).sendKeys("Daniel Doe");
+        driver.findElement(By.xpath("//p[text()='Viber']/following-sibling::button[1]")).click();
+        String expectedViber = "abc";
+        driver.findElement(By.xpath("//input[@placeholder='Viber']")).sendKeys(expectedViber);
+        WebElement driverLicExp = driver.findElement(By.name("driving_license_exp"));
+        driverLicExp.click();
+        driverLicExp.sendKeys("07232028");
+        WebElement medicalLicExp = driver.findElement(By.name("medical_certification_exp"));
+        medicalLicExp.click();
+        medicalLicExp.sendKeys("07232028");
+        driver.findElement(By.xpath("//button[text()='Create new']")).click();
+        driver.findElement(By.xpath("//button[text()='Go to Edit']")).click();
+        // Get Driver ID
+        String driverId = driver.findElement(By.xpath("//label[text()='Manual id']/following-sibling::div/input")).getAttribute("value");
+        driver.findElement(By.xpath("//button[text()='Back to list']")).click();
+        // Search for driver with ID
+        driver.findElement(By.xpath("//label[text()='Search...']/following-sibling::div/input")).sendKeys(driverId + Keys.ENTER);
+        // Click on non staff result
+        driver.findElement(By.xpath("//div[text()='Non staff']")).click();
+        // Getting driver id from search result
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@data-field='driver_number']/a")).click();
+        String actualViber = driver.findElement(By.xpath("//input[@placeholder='Viber']")).getAttribute("value");
+        Assert.assertEquals(actualViber, expectedViber);
+    }
+
+    @Test
+    public void createDriverOtherTestPositive() throws InterruptedException {
+        driver.get(ConfigReader.getProperty("elarappUrl"));
+        driver.findElement(By.id("login-username")).sendKeys(ConfigReader.getProperty("elarUsername"));
+        driver.findElement(By.id("login-password")).sendKeys(ConfigReader.getProperty("elarPassword"));
+        driver.findElement(By.xpath("//*[@id=\"login-form\"]/div/button")).click();
+        driver.findElement(By.xpath("//a[@href='/drivers/list']")).click();
+        driver.findElement(By.xpath("//button[text()='Add driver']")).click();
+        driver.findElement(By.xpath("//input[@name='full_name']")).sendKeys("Daniel Doe");
+        driver.findElement(By.xpath("//p[text()='Other']/following-sibling::button[1]")).click();
+        String expectedOther = "abc";
+        driver.findElement(By.xpath("//input[@placeholder='Other']")).sendKeys(expectedOther);
+        WebElement driverLicExp = driver.findElement(By.name("driving_license_exp"));
+        driverLicExp.click();
+        driverLicExp.sendKeys("07232028");
+        WebElement medicalLicExp = driver.findElement(By.name("medical_certification_exp"));
+        medicalLicExp.click();
+        medicalLicExp.sendKeys("07232028");
+        driver.findElement(By.xpath("//button[text()='Create new']")).click();
+        driver.findElement(By.xpath("//button[text()='Go to Edit']")).click();
+        // Get Driver ID
+        String driverId = driver.findElement(By.xpath("//label[text()='Manual id']/following-sibling::div/input")).getAttribute("value");
+        driver.findElement(By.xpath("//button[text()='Back to list']")).click();
+        // Search for driver with ID
+        driver.findElement(By.xpath("//label[text()='Search...']/following-sibling::div/input")).sendKeys(driverId + Keys.ENTER);
+        // Click on non staff result
+        driver.findElement(By.xpath("//div[text()='Non staff']")).click();
+        // Getting driver id from search result
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@data-field='driver_number']/a")).click();
+        String actualOther = driver.findElement(By.xpath("//input[@placeholder='Other']")).getAttribute("value");
+        Assert.assertEquals(actualOther, expectedOther);
+    }
+
 }
